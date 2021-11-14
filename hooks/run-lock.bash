ad6e2e0fb1d6daa61e62f2143bf1a530bf660521
Mykola Orliuk <virkony@gmail.com>
2021-11-14 16:58:26 +0100

    Integration with vlock/i3lock/swaylock

---
+++ hooks/run-lock.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Simple wrapper suitable for use with various locks like vlock/i3lock/swaylock
+# that allows to block return until screen unlocked.
+#
+# Example: ./run-lock.bash swaylock
+
+readonly events_dir="$(dirname "$0")/../events"
+trigger() { "$events_dir/trigger.bash" "$@"; }
+
+trigger locking
+start_second=$SECONDS
+
+"$@"
+status=$?
+
+LOCK_SECONDS=$(($SECONDS - $start_second)) trigger unlocked
+
+exit $status
