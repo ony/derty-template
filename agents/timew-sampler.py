ce622c9a6f98892992d51fc2aa77dc4678533689
Mykola Orliuk <virkony@gmail.com>
2021-11-16 09:58:28 +0100

    Account for open intervals in timew-sampler

--- agents/timew-sampler.py
+++ agents/timew-sampler.py
@@ -16,38 +16,47 @@ import isodate
 EVENTS_DIR = Path(sys.argv[0]).parent.parent.joinpath('events').resolve()
 
 
+def timew(*args) -> str:
+    return check_output(('timew',) + args).strip().decode('utf8')
+
+def parse_time(text) -> timedelta:
+    return timedelta(**{
+        k: float(v)
+        for (k, v) in zip(["seconds", "minutes", "hours"], text.split(':')[::-1])})
+
 def active_track() -> Optional[timedelta]:
-    if check_output(['timew', 'get', 'dom.active']).strip() != b'1':
+    if timew('get', 'dom.active') != '1':
         return None
-    duration = isodate.parse_duration(check_output(['timew', 'get', 'dom.active.duration']).strip().decode('utf8'))
+    duration = isodate.parse_duration(timew('get', 'dom.active.duration'))
     return timedelta(seconds=duration.total_seconds())
 
+def total_gaps_time(*args) -> timedelta:
+    total_line = [line.strip() for line in timew('gaps', *args).splitlines() if line][-1]
+    return parse_time(total_line)
 
-def total_gaps() -> timedelta:
-    total_line = [line.strip() for line in check_output(['timew', 'gaps']).splitlines() if line][-1]
-    gaps = timedelta(**{k: int(v)
-        for (k, v) in zip(
-            ["seconds", "minutes", "hours"],
-            total_line.split(b':')[::-1])})
-    return gaps
+def total_summary_time(*args) -> timedelta:
+    total_line = [line.strip() for line in timew('summary', *args).splitlines() if line][-1]
+    if 'No filtered' in total_line:
+        return timedelta(seconds=0)
+    else:
+        return parse_time(total_line)
 
+def remaining_gaps_time() -> timedelta:
+    return total_gaps_time(':blank') - total_summary_time()
 
 def notify(*args):
     check_call(['notify-send', '-h', 'string:x-canonical-private-synchronous:timew-hours'] + list(args))
 
-
 tomorrow = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
-next_reminder_hours = 2
+next_remainder_hours = 2
 while True:
     active = active_track()
-    reamaining_time = min(
-            total_gaps() - (active or timedelta(seconds=0)),
-            tomorrow - datetime.now())
-    reamaining_hours = reamaining_time.total_seconds() / 3600
-    if reamaining_hours <= 0:
+    time_left = min(remaining_gaps_time(), tomorrow - datetime.now())
+    hours_left = time_left.total_seconds() / 3600
+    if hours_left <= 0:
         check_call([EVENTS_DIR.joinpath('trigger.bash'), 'out-of-hours'])
         break
-    if active is not None and reamaining_hours <= next_reminder_hours:
-        notify(f'Only {reamaining_hours:.2f} hours left today')
-        next_reminder_hours = math.floor(reamaining_hours*4)/4
+    if active is not None and hours_left <= next_remainder_hours:
+        notify(f'Only {hours_left:.2f} hours left today')
+        next_remainder_hours = math.floor(hours_left*4)/4
     sleep(5 * 60)
